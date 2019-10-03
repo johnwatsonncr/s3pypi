520bba2548e8a4b4670ac4ad0bcc86f4abf82650
John Watson <john.watson.ncr@gmail.com>
2019-10-03 19:06:57 +0100

    adding cloudfront and package.json commands

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,16 +2,30 @@ import {
   CfnOutput, Construct, Stack, App, StackProps, RemovalPolicy, PhysicalName,
 } from '@aws-cdk/core';
 import { Bucket, BucketEncryption, } from '@aws-cdk/aws-s3';
+import { CloudFrontWebDistribution, HttpVersion, } from '@aws-cdk/aws-cloudfront';
+
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const bucket = new Bucket(this, 'pypiBucket', {
       encryption: BucketEncryption.S3_MANAGED,
-      bucketName: 'PyPiRepository20191003',
+      bucketName: 'pypi.repository.20191003',
       removalPolicy: RemovalPolicy.RETAIN,
       versioned: true
     });
+
+    const distribution = new CloudFrontWebDistribution(this, 'PyPiCFDistribution', {
+      originConfigs: [
+          {
+              s3OriginSource: {
+                  s3BucketSource: bucket
+              },
+              behaviors : [ {isDefaultBehavior: true}],
+              
+          }
+      ],
+   });
     new CfnOutput(this, 'pypiIndexUrl', {
       description: 'index url to add to pip config',
       exportName: 'pypiIndexUrl',
